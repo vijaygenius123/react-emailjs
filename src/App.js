c5fbfd9af546e03c8183c05cd1e8b650534412ab
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-10-14 18:12:37 +0100

    Integrate with emailjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import emailjs from '@emailjs/browser';
+import {useRef} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return (
+        <div className="App">
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name"/>
+                <label>Email</label>
+                <input type="email" name="user_email"/>
+                <label>Message</label>
+                <textarea name="message"/>
+                <input type="submit" value="Send"/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
